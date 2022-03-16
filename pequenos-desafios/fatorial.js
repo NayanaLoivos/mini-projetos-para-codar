0310e90e6f690b7a17f6d25e037248a065de2711
Nayana Loivos <loivosdev@gmail.com>
2022-03-15 21:54:59 -0300

    função primeiro caracter da string com switch case

--- pequenos-desafios/fatorial.js
+++ pequenos-desafios/fatorial.js
@@ -1,4 +1,4 @@
-    //Função fatorial de um número 
+    // Função fatorial de um número 
 
     function fatorial(n) {
         let resultado = n;
@@ -30,3 +30,26 @@
     }
 
     vogaisEConsoantes('javascriptloops');
+
+     // Primeiro caracter da string 
+    function getLetter(firstCharacter) {
+        let letter;
+        
+       switch(firstCharacter[0]){
+           case "a": case "e": case "i": case "o": case "u":
+           letter = 'A';
+           break;
+           case "b": case "c": case "d": case"f": case "g":
+           letter = 'B';
+           break;
+           case "h": case "j": case "k": case "l": case "m":
+           letter = 'C';
+           break;
+           case "n": case "p": case "q": case "r": case "s": case "t": 
+           case "v": case "w": case "x": case "y": case "z":
+           letter = 'D';
+           break;
+       }
+        return letter;
+    }
+    console.log(getLetter('adfst'));
